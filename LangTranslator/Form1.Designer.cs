2cad8508f224c2c1efab861b050e6e744a8c4ce5
Eugene <serpinin@mail.ru>
2019-02-23 14:11:56 +0400

    init

---
+++ LangTranslator/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace LangTranslator {
+	partial class MainForm {
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent() {
+            this.smallRegister = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.to = new System.Windows.Forms.ComboBox();
+            this.open = new System.Windows.Forms.Button();
+            this.translateButton = new System.Windows.Forms.Button();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.yandex = new System.Windows.Forms.LinkLabel();
+            this.customListLoadButton = new System.Windows.Forms.Button();
+            this.fileLabel = new System.Windows.Forms.Label();
+            this.listLabel = new System.Windows.Forms.Label();
+            this.fileNameLabel = new System.Windows.Forms.Label();
+            this.listNameLabel = new System.Windows.Forms.Label();
+            this.openList = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // smallRegister
+            // 
+            this.smallRegister.AutoSize = true;
+            this.smallRegister.Location = new System.Drawing.Point(44, 37);
+            this.smallRegister.Name = "smallRegister";
+            this.smallRegister.Size = new System.Drawing.Size(88, 17);
+            this.smallRegister.TabIndex = 20;
+            this.smallRegister.Text = "Small register";
+            this.smallRegister.UseVisualStyleBackColor = true;
+            this.smallRegister.CheckedChanged += new System.EventHandler(this.smallRegister_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "To";
+            // 
+            // to
+            // 
+            this.to.FormattingEnabled = true;
+            this.to.Location = new System.Drawing.Point(44, 10);
+            this.to.Name = "to";
+            this.to.Size = new System.Drawing.Size(121, 21);
+            this.to.TabIndex = 16;
+            // 
+            // open
+            // 
+            this.open.Location = new System.Drawing.Point(171, 39);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(75, 23);
+            this.open.TabIndex = 15;
+            this.open.Text = "Open";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // translateButton
+            // 
+            this.translateButton.Enabled = false;
+            this.translateButton.Location = new System.Drawing.Point(171, 10);
+            this.translateButton.Name = "translateButton";
+            this.translateButton.Size = new System.Drawing.Size(75, 23);
+            this.translateButton.TabIndex = 21;
+            this.translateButton.Text = "Translate";
+            this.translateButton.UseVisualStyleBackColor = true;
+            this.translateButton.Click += new System.EventHandler(this.translateButton_Click);
+            // 
+            // openFile
+            // 
+            this.openFile.Filter = "lang files|*.lang|txt files|*.txt";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Переведено сервисом";
+            // 
+            // yandex
+            // 
+            this.yandex.AutoSize = true;
+            this.yandex.Location = new System.Drawing.Point(121, 100);
+            this.yandex.Name = "yandex";
+            this.yandex.Size = new System.Drawing.Size(121, 13);
+            this.yandex.TabIndex = 23;
+            this.yandex.TabStop = true;
+            this.yandex.Text = "«Яндекс.Переводчик»";
+            this.yandex.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.yandex_LinkClicked);
+            // 
+            // customListLoadButton
+            // 
+            this.customListLoadButton.Location = new System.Drawing.Point(171, 68);
+            this.customListLoadButton.Name = "customListLoadButton";
+            this.customListLoadButton.Size = new System.Drawing.Size(75, 23);
+            this.customListLoadButton.TabIndex = 24;
+            this.customListLoadButton.Text = "Upload list";
+            this.customListLoadButton.UseVisualStyleBackColor = true;
+            this.customListLoadButton.Click += new System.EventHandler(this.customListLoadButton_Click);
+            // 
+            // fileLabel
+            // 
+            this.fileLabel.AutoSize = true;
+            this.fileLabel.Location = new System.Drawing.Point(12, 57);
+            this.fileLabel.Name = "fileLabel";
+            this.fileLabel.Size = new System.Drawing.Size(29, 13);
+            this.fileLabel.TabIndex = 25;
+            this.fileLabel.Text = "File: ";
+            // 
+            // listLabel
+            // 
+            this.listLabel.AutoSize = true;
+            this.listLabel.Location = new System.Drawing.Point(12, 78);
+            this.listLabel.Name = "listLabel";
+            this.listLabel.Size = new System.Drawing.Size(29, 13);
+            this.listLabel.TabIndex = 26;
+            this.listLabel.Text = "List: ";
+            // 
+            // fileNameLabel
+            // 
+            this.fileNameLabel.AutoSize = true;
+            this.fileNameLabel.Location = new System.Drawing.Point(36, 57);
+            this.fileNameLabel.Name = "fileNameLabel";
+            this.fileNameLabel.Size = new System.Drawing.Size(23, 13);
+            this.fileNameLabel.TabIndex = 27;
+            this.fileNameLabel.Text = "null";
+            // 
+            // listNameLabel
+            // 
+            this.listNameLabel.AutoSize = true;
+            this.listNameLabel.Location = new System.Drawing.Point(36, 78);
+            this.listNameLabel.Name = "listNameLabel";
+            this.listNameLabel.Size = new System.Drawing.Size(23, 13);
+            this.listNameLabel.TabIndex = 28;
+            this.listNameLabel.Text = "null";
+            // 
+            // openList
+            // 
+            this.openList.Filter = "txt files|*.txt";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 122);
+            this.Controls.Add(this.listNameLabel);
+            this.Controls.Add(this.fileNameLabel);
+            this.Controls.Add(this.listLabel);
+            this.Controls.Add(this.fileLabel);
+            this.Controls.Add(this.customListLoadButton);
+            this.Controls.Add(this.yandex);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.translateButton);
+            this.Controls.Add(this.smallRegister);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.to);
+            this.Controls.Add(this.open);
+            this.Name = "MainForm";
+            this.Text = "Lang Translator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.CheckBox smallRegister;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.ComboBox to;
+		private System.Windows.Forms.Button open;
+		private System.Windows.Forms.Button translateButton;
+		private System.Windows.Forms.OpenFileDialog openFile;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.LinkLabel yandex;
+        private System.Windows.Forms.Button customListLoadButton;
+        private System.Windows.Forms.Label fileLabel;
+        private System.Windows.Forms.Label listLabel;
+        private System.Windows.Forms.Label fileNameLabel;
+        private System.Windows.Forms.Label listNameLabel;
+        private System.Windows.Forms.OpenFileDialog openList;
+    }
+}
+
---
+++ LangTranslator/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Net;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace LangTranslator {
+	public partial class MainForm : Form {
+		YandexTranslator translator = new YandexTranslator();
+		Dictionary<string, string> dic = new Dictionary<string, string>();
+        Dictionary<string, string> userDic = new Dictionary<string, string>();
+		bool small;
+
+		public MainForm() {
+			InitializeComponent();
+			dic.Add("ru_RU", "ru");
+			dic.Add("en_US", "en");
+			dic.Add("ar_SA", "ar");
+			dic.Add("az_AZ", "az");
+			dic.Add("de_DE", "de");
+			dic.Add("eo_UY", "eo");
+			dic.Add("es_ES", "es");
+			dic.Add("fr_FR", "fr");
+			dic.Add("ga_IE", "ga");
+			dic.Add("gd_GB", "gd");
+			dic.Add("hy_AM", "hy");
+			dic.Add("it_IT", "it");
+			dic.Add("ja_JP", "ja");
+			dic.Add("pl_PL", "pl");
+			dic.Add("pt_PT", "pt");
+			dic.Add("uk_UA", "uk");
+			dic.Add("zh_CN", "zh");
+
+			List<string> keys = new List<string>(dic.Keys);
+			for (int i = 0; i < dic.Count; i++)
+				to.Items.Add(keys[i]);
+		}
+
+		private void open_Click(object sender, EventArgs e) {
+			DialogResult result = openFile.ShowDialog();
+			if (result != DialogResult.Cancel) {
+				translateButton.Enabled = true;
+                fileNameLabel.Text = openFile.SafeFileName.Length <= 15 ? openFile.SafeFileName : openFile.SafeFileName.Substring(0, 15) + "...";
+            }
+		}
+
+        private void customListLoadButton_Click(object sender, EventArgs e) {
+            DialogResult result = openList.ShowDialog();
+            if (result != DialogResult.Cancel) {
+                string[] file = File.ReadAllLines(openList.FileName);
+                for (int i = 0; i < file.Length; i++) {
+                    string[] splited = file[i].Split(new char[] { '=' }, StringSplitOptions.RemoveEmptyEntries);
+                    userDic.Add(splited[0], splited.Length == 1 ? "empty" : splited[1]);
+                }
+                listNameLabel.Text = openList.SafeFileName.Length <= 15 ? openList.SafeFileName : openList.SafeFileName.Substring(0, 15) + "...";
+            }
+        }
+
+        private void smallRegister_CheckedChanged(object sender, EventArgs e) {
+			small = smallRegister.Checked;
+		}
+
+		private void translateButton_Click(object sender, EventArgs e) {
+			if (dic.ContainsKey(to.Text)) {
+				if (File.Exists(openFile.FileName)) {
+					string[] file = File.ReadAllLines(openFile.FileName);
+					string[] result = new string[file.Length];
+					for (int i = 0; i < file.Length; i++) {
+						if (file[i].Contains("=")) {
+							string[] splited = file[i].Split(new char[] { '=' }, StringSplitOptions.RemoveEmptyEntries);
+                            if (splited.Length == 1) {
+                                result[i] = splited[0] + "=";
+                                continue;
+                            }
+
+                            string resultText = "";
+                            string[] secondSplited = SplitAndKeep(splited[1], userDic.Keys.ToArray()).ToArray();
+                            for (int j = 0; j < secondSplited.Length; j++) {
+                                string toAdd = secondSplited[j];
+                                string value = null;
+                                try { value = userDic[toAdd]; } catch (Exception d) { }
+                                resultText += value == null ? translator.Translate(toAdd, dic[to.Text]) : value.Equals("empty") ? toAdd : value;
+                            }
+                            result[i] = splited[0] + "=" + resultText;
+						}
+					}
+					File.WriteAllLines(openFile.FileName.Substring(0, openFile.FileName.Length - openFile.SafeFileName.Length) + (small ? to.Text.ToLower() : to.Text) + ".lang", result);
+					MessageBox.Show("Успешно переведенно!");
+				}
+				else
+					MessageBox.Show("Указанного lang файла не существует", "Ошибка");
+			}
+			else
+				MessageBox.Show("Введите язык правильно", "Ошибка");
+		}
+
+		private void yandex_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
+			System.Diagnostics.Process.Start("https://translate.yandex.ru/");
+		}
+
+        static IEnumerable<string> SplitAndKeep(string s, params string[] delims) {
+            var rows = new List<string>() { s };
+            foreach (string delim in delims)//delimiter counter
+            {
+                for (int i = 0; i < rows.Count; i++)//row counter
+                {
+                    int index = rows[i].IndexOf(delim);
+                    if (index > -1
+                        && rows[i].Length > index + 1 && !rows[i].Equals(delim)) {
+                        string leftPart = rows[i].Substring(0, index);
+                        string centerPart = rows[i].Substring(index, delim.Length);
+                        string rightPart = rows[i].Substring(index + delim.Length);
+                        rows[i] = leftPart;
+                        rows.Insert(i + 1, centerPart);
+                        rows.Insert(i + 2, rightPart);
+                    }
+                }
+            }
+            return rows;
+        }
+    }
+
+    class YandexTranslator {
+		public string Translate(string s, string lang) {
+			if (s.Length > 0) {
+				WebRequest request = WebRequest.Create("https://translate.yandex.net/api/v1.5/tr.json/translate?"
+					+ "key=trnsl.1.1.20170731T101955Z.360d16f26201aaf1.74a0721586017a1c6144f87a54a9092d9e9765d3"
+					+ "&text=" + s
+					+ "&lang=" + lang);
+
+				WebResponse response = request.GetResponse();
+
+				using (StreamReader stream = new StreamReader(response.GetResponseStream())) {
+					string line;
+
+					if ((line = stream.ReadLine()) != null) {
+						Translation translation = JsonConvert.DeserializeObject<Translation>(line);
+
+						s = "";
+
+						foreach (string str in translation.text) {
+							s += str;
+						}
+					}
+				}
+
+				return s;
+			}
+			else
+				return "";
+		}
+	}
+
+	class Translation {
+		public string code { get; set; }
+		public string lang { get; set; }
+		public string[] text { get; set; }
+	}
+}
